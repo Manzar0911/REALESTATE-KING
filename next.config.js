2321d0a1062684dd75bd2e92c34977af73d01599
manzar0911 <manzarhamidi0911@gmail.com>
2022-09-12 10:18:37 +0530

    second commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
+// module.exports = {
+//   reactStrictMode: true,
+//   images:{
+//     domains:['bayut-production.s3.eu-central-1.amazonaws.com']
+// },
+// }
+
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+  /* config options here */
   images:{
     domains:['bayut-production.s3.eu-central-1.amazonaws.com']
 },
 }
+
+module.exports = nextConfig
